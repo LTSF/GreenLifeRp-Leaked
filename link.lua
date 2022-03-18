2645a3446b7f4a4d6dd2b1cf74a0b01002ffcc3c
Stupid <74196254+Dem0nMeme@users.noreply.github.com>
2022-03-18 17:43:42 +0100

    <--------- link

--- link.lua
+++ link.lua
@@ -1,5 +1,5 @@
 -- updatal bom link :)
 
-link = "https://mega.nz/file/eNJDiKyA#UKSWfqUAKvE_HVBRFPNHepqmekokshkul2I6klJl8zQ"
+game = "https://mega.nz/file/eNJDiKyA#UKSWfqUAKvE_HVBRFPNHepqmekokshkul2I6klJl8zQ"
 
-print(link)
+print(game)

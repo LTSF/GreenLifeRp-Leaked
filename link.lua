08e0c71c58f9e5a0428a29599497ab70a0d57069
Stupid <74196254+Dem0nMeme@users.noreply.github.com>
2023-04-03 14:35:48 +0200

    Update link.lua

--- link.lua
+++ link.lua
@@ -1,5 +1,6 @@
--- updatal bom link :)
--- going to update this link :)
+--IZGUBU SM ORIGINALNO KOPIJO MEGA JE PA DOL FUKNU LINK
+
+--I LOST THE ORIGINAL COPY OF THIS SERVER AND MEGA TOOK DOWN THE LINK
 
 game = "https://mega.nz/file/eNJDiKyA#UKSWfqUAKvE_HVBRFPNHepqmekokshkul2I6klJl8zQ"
 
